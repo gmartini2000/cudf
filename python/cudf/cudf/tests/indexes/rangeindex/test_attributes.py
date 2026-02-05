ad9a7458325cf8f4ed4a9d2a2dd9d458160e7cdf
gmartini2000 <martinigiulio02@gmail.com>
2026-02-05 23:37:35 +0000

    pre PR

--- python/cudf/cudf/tests/indexes/rangeindex/test_attributes.py
+++ python/cudf/cudf/tests/indexes/rangeindex/test_attributes.py
@@ -8,6 +8,47 @@ import cudf
 from cudf.testing import assert_eq
 from cudf.testing._utils import assert_exceptions_equal
 
+import inspect
+import numpy as np
+import cudf
+
+def test_rangeindex_to_numpy_signature():
+    sig = inspect.signature(cudf.RangeIndex.to_numpy)
+    assert list(sig.parameters) == ["self", "dtype", "copy", "na_value"]
+    assert sig.parameters["dtype"].default is None
+    assert sig.parameters["copy"].default is False
+    assert sig.parameters["na_value"].default is None
+
+def test_rangeindex_to_numpy_caches_copy_false():
+    idx = cudf.RangeIndex(0, 10, 1)
+    a = idx.to_numpy(copy=False)
+    b = idx.to_numpy(copy=False)
+    assert a is b
+
+def test_rangeindex_to_numpy_copy_true():
+    idx = cudf.RangeIndex(0, 10, 1)
+    a = idx.to_numpy(copy=False)
+    b = idx.to_numpy(copy=True)
+    assert a is not b
+    assert np.array_equal(a, b)
+
+def test_rangeindex_to_numpy_dtype():
+    idx = cudf.RangeIndex(0, 10, 1)
+    a = idx.to_numpy(dtype=np.int32, copy=False)
+    assert a.dtype == np.int32
+
+def test_rangeindex_to_numpy_accepts_na_value():
+    idx = cudf.RangeIndex(0, 3, 1)
+    a = idx.to_numpy(na_value=-1)
+    assert a.tolist() == [0, 1, 2]
+
+def test_rangeindex_values_host_routes_to_to_numpy_cache():
+    idx = cudf.RangeIndex(0, 10, 1)
+    a = idx.values_host
+    b = idx.values_host
+    assert a is b
+    assert np.array_equal(a, idx.to_numpy(copy=False))
+
 
 def test_rangeindex_contains():
     ridx = cudf.RangeIndex(start=0, stop=10, name="Index")
